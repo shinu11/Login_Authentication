cccb363353a0b1a47ef374dc42bce792c2a28f34
Shinu kp <shinunandu123@gmail.com>
2023-03-21 11:37:44 +0530

    signup views.py completed

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,8 @@
+
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 
@@ -8,6 +11,25 @@ def home(request):
     return render(request,'index.html')
 
 def signup(request):
+
+    if request.method == "POST":
+        username = request.POST['username']
+        fname =    request.POST['fname']
+        lname =    request.POST['lname']
+        email =    request.POST['email']
+        p1    =    request.POST['p1']
+        p2    =    request.POST['p2']
+
+        myuser=User.objects.create_user(username,email,p1)
+        myuser.first_name=fname
+        myuser.last_name=lname
+        
+        myuser.save()
+
+        messages.success(request,"Your account has been succefully created")
+        return redirect('signin')
+
+
     return render(request,'signup.html')
 
 def signin(request):
